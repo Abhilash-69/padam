66979d66668f147d7c29aadb8ec62bfd6d62457a
samridhaa <samridhaaraja@gmail.com>
2024-04-06 08:10:59 +0530

    many

--- server/database.sql
+++ server/database.sql
@@ -2,13 +2,15 @@ drop table if EXISTS USER_REVIEW;
 drop table if EXISTS WATCH;
 drop table if EXISTS ACTS;
 drop table if EXISTS MOVIE_IMAGE;
-drop table if EXISTS MOVIE;
 drop table if EXISTS USERs;
 drop table if EXISTS OTT_NAME;
 drop table if EXISTS CREW_IMAGE;
 drop table if EXISTS CREW_NAME;
 drop table if EXISTS CREW;
 drop table if EXISTS OTT;
+drop table if EXISTS MOVIE;
+drop table if EXISTS ACTOR;
+
 CREATE TABLE IF NOT EXISTS MOVIE(
     m_id text PRIMARY KEY,
     m_name varchar(255),
@@ -28,49 +30,45 @@ CREATE TABLE IF NOT EXISTS MOVIE(
 
 CREATE TABLE IF NOT EXISTS MOVIE_IMAGE
 (
-     m_id TEXT ,
-     FOREIGN KEY (m_id) REFERENCES MOVIE (m_id),
-     m_image TEXT
+    img_id SERIAL PRIMARY KEY,
+    m_id TEXT ,
+    FOREIGN KEY (m_id) REFERENCES MOVIE (m_id),
+    m_image TEXT
 
 );
+
+-- CREATE TABLE IF NOT EXISTS ACTOR(
+--     c_name text PRIMARY KEY,
+--     birthday text,
+--     birthplace text,
+--     about text,
+--     c_image text
+-- );      
+
+
 
 CREATE TABLE IF NOT EXISTS CREW(
-    c_id SERIAL PRIMARY KEY,
-    c_name text,
-    birthday text,
-    birthplace text,
-    about text,
-    c_image text
-);
-
-
-
-CREATE TABLE IF NOT EXISTS ACTS(
     m_id text ,
     FOREIGN KEY (m_id) REFERENCES MOVIE (m_id),
-    c_id int ,
-    Foreign Key (c_id) REFERENCES CREW(c_id),
-    character TEXT,
-    cc_image TEXT
-
-);
+    c_name text  ,
+    -- Foreign Key (c_name) REFERENCES ACTOR(c_name),
+    -- Primary Key(m_id,c_name),
+); 
 
 CREATE TABLE IF NOT EXISTS OTT(
-    o_id SERIAL PRIMARY KEY
+    o_name Text Primary Key,
+    o_link Text 
 );
 
-CREATE TABLE IF NOT EXISTS OTT_NAME(
-       o_id int ,
-       Foreign Key (o_id) REFERENCES OTT(o_id),
-       o_name TEXT
-);
 
 CREATE TABLE IF NOT EXISTS WATCH(
 
      m_id TEXT ,
      FOREIGN KEY (m_id) REFERENCES MOVIE (m_id),
-     o_id int ,
-     Foreign Key (o_id) REFERENCES OTT(o_id)
+     o_name TEXT,
+     Foreign Key (o_name) REFERENCES OTT(o_name),
+     Primary Key(m_id,o_name)
+
 
 );
 
@@ -80,22 +78,21 @@ CREATE TABLE IF NOT EXISTS USERS(
     u_name varchar(50),
     email text,
     passwd text
-
 );
 
 CREATE TABLE IF NOT EXISTS USER_REVIEW(
     r_id SERIAL PRIMARY KEY,
     m_id TEXT ,
     FOREIGN KEY (m_id) REFERENCES MOVIE (m_id),
-    u_id INT,
+    u_id TEXT,
     FOREIGN KEY (u_id) REFERENCES USERS(u_id),
     review TEXT
-
 );
 
 
+ DELETE FROM MOVIE WHERE M_ID='leo_2023_2';
 
-
+SELECT * from MOVIE;
 
 
 
--- server/index.js
+++ server/index.js
@@ -1,4 +1,4 @@
-const express = require("express");
+/*const express = require("express");
 const app = express();
 const cors = require("cors");
 const db = require("./database");
@@ -38,6 +38,67 @@ app.get("/api/m/:movie_name",async (req,res)=>{
         const poster =  await scrap_movie_img(movie_name);
         const info = await scrap_movie_info(movie_name);
         const s = await db.query("insert into movie(m_id,m_name,synopsis,poster,genre,original_language,director,producer,writer,release_date_theaters,runtime,distributor,production_co,aspect_ratio) values($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14) RETURNING *",[movie_name,mov,synop,poster,info.Genre,info.Original_Language,info.Director,info.Producer,info.Writer,info.Release_Date,info.Runtime,info.Distributor,info.Production_Co,info.Aspect_Ratio]);       
+        res.json(s.rows[0])
+    }
+    else{
+        console.log(resp.rowCount)
+        res.json(resp.rows[0])
+    }
+})
+
+app.listen(8000,()=>{
+    console.log("Listening on port 8000");
+})
+*/
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const db = require("./database");
+const {scrap_movie_name} = require("./scrap")
+const {scrap_movie_info} = require("./scrap")
+const {scrap_synopsis} = require("./scrap")
+const {scrap_movie_img} = require("./scrap")
+const {scrap_cast_and_crew} = require("./scrap");
+const {scrap_movie_photos} = require("./scrap")
+const {scrap_actor_info} = require("./castScrap");
+const {scrap_ott} = require("./scrap");
+const {v4} = require("uuid4"); 
+app.use(cors());
+app.use(express.json())
+/*
+app.get("/api/a/:actor_name",async (req,res)=>{
+    const actor_name = req.params.actor_name;
+    console.log(actor_name);
+    let resp = await db.query(`select * from actor where c_name='${actor_name}'`);
+    if(resp.rowCount===0){
+        console.log("scrap")
+        const obj= await scrap_actor_info(actor_name);
+        //const img=await scrap_cast_and_crew(movie_name);
+        const s = await db.query("insert into actor(c_name,birthday,birthplace,about) values($1,$2,$3,$4) RETURNING *",[actor_name,obj.actor_bday,obj.actor_bplace,obj.actor_bio]);        
+        res.json(s.rows[0])
+    }
+    else{
+        res.json(resp.rows[0])
+    }
+})
+*/
+app.get("/api/m/:movie_name",async (req,res)=>{
+    const movie_name = req.params.movie_name;
+    console.log(movie_name);
+    let resp = await db.query(`select * from movie where m_id='${movie_name}'`);
+    if(resp.rowCount===0){
+        console.log("scrap")
+        const mov= await scrap_movie_name(movie_name);
+        const synop = await scrap_synopsis(movie_name);
+        const poster =  await scrap_movie_img(movie_name);
+        const info = await scrap_movie_info(movie_name);
+        const images = await scrap_movie_photos(movie_name);
+        const crew = await scrap_cast_and_crew(movie_name);
+        console.log(movie_name,crew.actorName,crew.characterName,crew.imageUrl)
+        const ott = await scrap_ott(movie_name);
+        const s = await db.query("insert into movie(m_id,m_name,synopsis,poster,genre,original_language,director,producer,writer,release_date_theaters,runtime,distributor,production_co,aspect_ratio) values($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14) RETURNING *",[movie_name,mov,synop,poster,info.Genre,info.Original_Language,info.Director,info.Producer,info.Writer,info.Release_Date,info.Runtime,info.Distributor,info.Production_Co,info.Aspect_Ratio]);       
+        const b = await db.query("insert into movie_image(m_id,m_image) values($1,$2) RETURNING *",[movie_name,images]);
+        const c = await db.query("insert into crew(m_id,c_name,char_role,cc_image) values($1,$2,$3,$4) RETURNING *",[movie_name,crew.actorName,crew.characterName,crew.imageUrl]);
         res.json(s.rows[0])
     }
     else{
